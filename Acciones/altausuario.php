8b39a048fe94907584de67387f4694e86fc854df
Santiago Duran <Santiagoduran2006@gmail.com>
2024-09-11 23:34:25 -0300

    asd

--- Acciones/altausuario.php
+++ Acciones/altausuario.php
@@ -1,13 +1,18 @@
 <?php 
-    include("ConexionBD.php");
+    include_once("ConexionBD.php");
     
-    $conexion = mysqli_connect("localhost","root","","ecounlz");
+    //$conexion = mysqli_connect("localhost","root","","ecounlz");
     if(!empty($_POST["crear"])){
-       if(!empty($_POST["DNI"])){
+       if(!empty($_POST["DNI"]) or !empty($_POST["Nombre"]) or !empty($_POST["Apellido"]) or !empty($_POST["Email"]) or !empty($_POST["Clave"])){
             $DNI = $_POST["DNI"];
-            $consulta=$conexion->query("INSERT INTO Usuarios(DNI) VALUES ('$DNI')");
-
+            $Nombre = $_POST["Nombre"];
+            $Apellido = $_POST["Apellido"];
+            $Email = $_POST["Email"];
+            $Clave = $_POST["Clave"];
+            $consulta=$conexion->query("INSERT INTO Usuarios(DNI,Nombre,Apellido,Correo,Clave) VALUES ('$DNI','$Nombre','$Apellido','$Email','$Clave')");
+            mysqli_close($conexion);
        }
+      
     }
-   
+    
 ?>
