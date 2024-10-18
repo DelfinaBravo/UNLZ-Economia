38a61126bec4169ecd95fa67ba331aa9b28b84bc
Santiago Duran <Santiagoduran2006@gmail.com>
2024-10-18 14:44:08 -0300

    validaciones

--- crearusuario.php
+++ crearusuario.php
@@ -61,7 +61,8 @@
 
                 <div class="casilleross">
                     <!-- Correo -->
-                    <input type="email" name="email" id="Email" placeholder="Ingrese su Email">
+                    <input type="email" name="email" id="Email" placeholder="Ingrese su Email"
+                    required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
                 </div>
 
                 <div class="casilleross">
