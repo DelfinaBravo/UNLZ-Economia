d47be2e692d1efb66f4de38a5a890d78843906b4
agusberal <laumaldo006@gmail.com>
2024-10-20 01:52:38 -0300

    cambiar contraseña

--- cambiarContraseña.php
+++ cambiarContraseña.php
@@ -32,7 +32,7 @@
 </head>
 <body class="body">
     <?php 
-        include("Acciones/login.php");
+        include("Acciones/cambiarCon.php");
     ?>
     <!-- Metodología BEM -->
     <header>
@@ -48,28 +48,22 @@
                         <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                             <path fill="#A60A6C" fill-opacity="1" d="M0,64L48,85.3C96,107,192,149,288,176C384,203,480,213,576,192C672,171,768,117,864,106.7C960,96,1056,128,1152,160C1248,192,1344,224,1392,240L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
                         </svg>
-                    <h1>Hola de nuevo!</h1>
-                        <div class="casilleros">
-                            <!-- Email -->
-                            <!-- required pattern es para garantizar que el email tenga un formato valido. -->
-                            <input type="email" name="email" id="email" placeholder="Ingrese su Email"
-                            required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+                        <h1>Vamos de vuelta!</h1>
+                        <div class="casilleross">
+                            <!-- Clave -->
+                            <input type="password" name="claveNueva"  placeholder="Ingrese su contraseña">
                         </div>
-                        <div class="casilleros">
-                                <!-- Contraseña -->
-                                <!-- required minlength es para que tenga que ingresar minimo 8 caracteres -->
-                            <input type="password" name="clave" id="clave" placeholder="Ingrese su Contraseña"
-                            required minlength="8">
-                            <a href="recuperarContra.php" class="passwordforgot">Olvide mi contraseña</a></p>
+
+                        <div class="casilleross">
+                                <!-- Confirmar Clave -->
+                            <input type="password" name="claveNuevaRep"  placeholder="Confirme su contraseña">
                         </div>
+                       
                         <div class="botonusu">
                                 <!-- Botón -->
-                            <input class="botonusuario" type="submit" value="Iniciar Sesión" name="ingresar">
+                            <input class="botonusuario" type="submit" value="cambiar contraseña" name="cambiar">
                         </div>
-                        <div class="linkusuario">
-                            <!-- Link para crear cuenta -->
-                        <p>¿No tenes cuenta? <a href="crearusuario.php"> ¡Registrate!</a></p>
-                        </div>
+                        
                     </form>
             </div>
     </main>
