3061e513e2cbf25c6b62e382e1e5376bd4ec4012
lazaroperv <lazaroperv18@gmail.com>
2024-11-28 13:59:57 -0300

    l,

--- VistasAdmin/inicioA.php
+++ VistasAdmin/inicioA.php
@@ -46,25 +46,26 @@ if (isset($_POST['cerrar'])) {
 
     <!-- Meta descripción (máx. 160 caracteres) -->
     <meta name="description" content="Proyecto educativo para mejorar la página de Economía de la UNLZ realizado por estudiantes de la Técnica N*1 de Esteban Echeverría.">
-    <link rel="stylesheet" href="../Estilos/style.css">
+    <link rel="stylesheet" href="../Estilos/style-admin.css">
     <!-- Título -->
     <title>Univerdad de Economía</title>
     <!-- Estilos Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 </head>
-<body>
+<body class="body-admin">
     <!-- Metodología BEM -->
     <header>
         <div class="menu">
             <nav>
                 <a class="logo" href="index.html"><img src="../Media/logoUNLZ.jpeg" alt="logoUNLZ" width="75px"></a>
-                <form id="form-cerrar-sesion" class="form-sesion" action="" method="post">
-                    <input type="submit" value="Cerrar Sesión" name="cerrar" >
-                </form>
+                    <ul class="links">
+                        <a href="">Salir</a>
+                    </ul>
             </nav>
         </div>
     </header>
+
     <main>
     <div class="bienvenida"></div>
             <h1>Bienvenido Administrador <?php echo $_SESSION['nombre_completo'];?> </h1>
@@ -86,30 +87,6 @@ if (isset($_POST['cerrar'])) {
     </main>
     <footer>
     <footer>
-        <div class="grid">
-            <div class="item"><p>Contacto</p>
-                <div class="info">
-                    <p>Camino de Cintura y Juan XXIII</p>
-                    <p>Lomas de Zamora</p>
-                    <p>Buenos Aires - Argentina</p>
-                    <p>economicas@unlz.edu.ar</p>
-                    <p> 2152-8430</p>
-                </div>
-            </div>
-            <div class="item"><p>Redes Sociales</p>
-                    <div class="redes">
-                        <a href=""> <img class="imgredes" src="../Media/youtube.png" alt="" width="50px"></a>
-                        <a href=""><img class="imgredes" src="../Media/instagram.png" alt="" width="50px"></a>
-                        <a href=""><img class="imgredes" src="../Media/facebook.png" alt="" width="50px"></a> 
-                    </div>
-            </div>
-            <div class="item"><p>Ubicacion</p>
-                <div class="mapa">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.1912862628506!2d-58.461163959944194!3d-34.77595947300585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd21ee8f2942f%3A0x4dc91fe7512a3137!2sFacultad%20de%20Ciencias%20econ%C3%B3micas%20-%20UNLZ!5e0!3m2!1sen!2sar!4v1726123359353!5m2!1sen!2sar" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
-        </div>
-       
          <div class="derechos">
             <p>_____________________________________________________________________________________________________________________________________________</p>
             <p>&copy; 2016 | UNLZ - Facultad de Ciencias Económicas |<a href="http://www.economicas.unlz.edu.ar/"> www.economicas.unlz.edu.ar</a> </p>
